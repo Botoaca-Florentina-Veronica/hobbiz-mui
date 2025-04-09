080f8fd0f572c6e154e1b3fd3514fa6e0a2093c9
Vera <veraflorentina01@gmail.com>
2025-04-09 20:17:58 +0300

    adaugare netlify.toml

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    rollupOptions: {
+      external: [
+        'react-slick',
+        'slick-carousel/slick/slick.css',
+        'slick-carousel/slick/slick-theme.css'
+      ]
+    }
+  },
+  optimizeDeps: {
+    include: ['react-slick', 'slick-carousel']
+  }
 })
