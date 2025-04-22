2b5b48fb15ac00807c60137740d2d7cf1f9a3ea9
Vera <veraflorentina01@gmail.com>
2025-04-22 11:53:26 +0300

    modificari la darkmode

--- frontend/src/components/MainStage.jsx
+++ frontend/src/components/MainStage.jsx
@@ -46,7 +46,7 @@ export default function MainStage() {
       {/* Conținutul principal */}
       <div className="main-content">
         <div className="main-text">
-          <h1 id="main-title">Ai vreun hobby fain și crezi că e inutil? Poate îl găsești chiar aici! Fă și un ban cinstit cu el!</h1>
+          <h1 id="main-title">Ai vreun hobby fain și crezi că e inutil? Găsește oameni care sunt dispuși să plătească pentru asta!</h1>
           <p>Fă din pasiunea ta o sursă de venit!</p>
           <button className="sign-up-button">Sign up</button>
         </div>
